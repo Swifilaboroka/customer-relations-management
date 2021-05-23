a49388dfded916cb6a09005e68383d6e207f8063
evakuator <kuatabylkasymov@gmail.com>
2021-05-23 18:36:24 +0600

    filter agents for current organization

--- agents/urls.py
+++ agents/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     # path('update/<int:pk>/', lead_update, name='lead-update'),
     path('list/', AgentListView.as_view(), name='agent-list'),
     path('create/', AgentCreateView.as_view(), name='agent-create'),
-    path('detail/<int:pk>/', agent_detail, name='agent-detail'),
+    path('detail/<int:pk>/', AgentDeleteView, name='agent-detail'),
     path('update/<int:pk>/', AgentUpdateView.as_view(), name='agent-update'),
     path('delete/<int:pk>/', AgentDeleteView.as_view(), name='agent-delete'),
 ]
--- agents/views.py
+++ agents/views.py
@@ -1,7 +1,6 @@
 from django.urls.base import reverse
 from django.views.generic.edit import DeleteView, UpdateView
-from django.shortcuts import get_object_or_404, redirect, render
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView,DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from leads.models import Agent
 from .forms import AgentForm
@@ -12,7 +11,8 @@ class AgentListView(LoginRequiredMixin, ListView):
     context_object_name = 'agents'
 
     def get_queryset(self):
-        return Agent.objects.all()
+        organization = self.request.user.user_profile
+        return Agent.objects.filter(organization=organization)
 
 
 class AgentCreateView(LoginRequiredMixin, CreateView):
@@ -27,14 +27,21 @@ class AgentCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-def agent_detail(request, pk):
-    agent = get_object_or_404(Agent, pk=pk)
-    return render(request=request, template_name='agents/agent_detail.html', context={"agent": agent})
+class AgentDetailView(LoginRequiredMixin, DetailView):
+    template_name = 'agents/agent_detail.html'
+
+    def get_queryset(self):
+        organization = self.request.user.user_profile
+        return Agent.objects.filter(organization=organization)
 
 class AgentUpdateView(LoginRequiredMixin, UpdateView):
     template_name = 'agents/agent_update.html'
     form_class = AgentForm
     queryset = Agent.objects.all()
+
+    def get_queryset(self):
+        organization = self.request.user.user_profile
+        return Agent.objects.filter(organization=organization)
 
     def get_success_url(self) -> str:
         return reverse('agents:agent-list')
@@ -42,6 +49,10 @@ class AgentUpdateView(LoginRequiredMixin, UpdateView):
 class AgentDeleteView(LoginRequiredMixin, DeleteView):
     queryset = Agent.objects.all()
     template_name = 'agents/agent_delete.html'
+
+    def get_queryset(self):
+        organization = self.request.user.user_profile
+        return Agent.objects.filter(organization=organization)
 
     def get_success_url(self) -> str:
         return reverse('agents:agent-list')
